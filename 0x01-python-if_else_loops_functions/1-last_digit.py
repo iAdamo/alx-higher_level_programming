3a0f5436c4dff050e4e57f52d1497d9e3305804b
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-09 11:12:55 +0100

    Last digit: fixed typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
+if number < 0:
+    lastDigit *= -1
 if lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif lastDigit == 0:
