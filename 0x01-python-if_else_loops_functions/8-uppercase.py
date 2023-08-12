9f9ad065da7fea7495d5e38788594ec715bee175
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-12 03:25:08 +0100

    convert to Uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
     for each_chr in str:
         deca = ord(each_chr)
         if deca in range(97, 123):
-            deca -= 32
-        print("{}".format(chr(deca)), end='')
+            deca = chr(deca - 32)
+        print("{}".format(deca), end='')
     print("")
