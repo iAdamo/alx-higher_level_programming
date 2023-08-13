044fc244982cbc39833ab5d20fc280ecef71401d
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-13 17:25:22 +0100

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,8 +7,17 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
+    operator = "+-*/"
+    flag = 0
+    for each_op in operator:
+        if each_op == argv[2]:
+            flag = 1
+    if flag == 0:
+        print("Unknown operator. Available operators: +, -, * and / ")
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
+
     match argv[2]:
         case "+":
             print(f"{a} {argv[2]} {b} = {add(a, b)}")
@@ -17,7 +26,4 @@ if __name__ == "__main__":
         case "*":
             print(f"{a} {argv[2]} {b} = {mul(a, b)}")
         case "/":
-            print(f"{a} {argv[2]} {b} = {div(a, b)}")
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            print(f"{a} {argv[2]} {b} = {div(a, b)}") 
