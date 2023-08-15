ea5a92bd64c35f9e1fe3d40789de984b65d867a8
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-15 01:20:32 +0100

    Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def element_at(my_list, idx):
+    """Function that retrieves an element from a list like in C.
+
+    Args:
+        my_list: list of integers
+        idx: position index
+
+    Retuns:
+        Element at at position idx
+    """
+    if idx < 0 or len(my_list) < idx:
+        return None
+    return my_list.pop(idx)
