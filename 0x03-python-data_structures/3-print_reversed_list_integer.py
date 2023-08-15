ddfa69aa1907993edce91cdf8a153357b9769f4c
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-15 02:36:36 +0100

    for each_int in my_list:

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Function that prints all integers of a list, in reverse order
+
+        Args:
+            my_list: list of integers
+
+        Return:
+            None
+    """
+    my_list.reverse()
+    for each_int in my_list:
+        print("{:d}".format(each_int))
