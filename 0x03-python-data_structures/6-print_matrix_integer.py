7c751c75d0876708605025a8d9bca24ea255f2ff
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-16 23:53:40 +0100

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -13,7 +13,7 @@ def print_matrix_integer(matrix=[[]]):
         None
     """
     if matrix == [[]]:
-        print()
+        print("".format())
     else:
         n = len(matrix)
         for each_item in matrix:
