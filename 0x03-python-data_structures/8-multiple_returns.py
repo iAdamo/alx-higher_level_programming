cc4a84ec7b2e633cec86a98f4114ca057a3c6967
Adam <iadamo.inc@gmail.com>
2023-08-17 01:12:03 -0700

    fixed typo to task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -8,4 +8,4 @@ def multiple_returns(sentence):
         first_char = None
     else:
         first_char = sentence[0]
-        return (len(sentence), first_char)
+    return (len(sentence), first_char)
