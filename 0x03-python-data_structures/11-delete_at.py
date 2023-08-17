036850eb0abce3d46356f2a4cb8a8f1fe85354bf
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-17 13:11:27 +0100

    delete an index position

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,12 +4,21 @@ if __name__ != "__main__":
 
 
 def delete_at(my_list=[], idx=0):
-    if len(mylist) == 0:
+    """  function that deletes the item at a specific position in a list
+
+        Args:
+        my_list: list of integers
+        idx: index position to delete
+
+        Return:
+        a new list
+
+    """
+    new_list = []
+    if len(my_list) == 0:
         return None
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    for i in range(len(my_list)):
-        if i == idx:
-            continue
-        new_list.append(my_list[i])
+    del my_list[idx]
+    new_list = my_list.copy()
     return (new_list)
