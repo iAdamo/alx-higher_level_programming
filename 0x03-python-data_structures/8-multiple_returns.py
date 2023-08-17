c15e83f77a2b8e2c505ff814913dd0a5992fe825
Adam <iadamo.inc@gmail.com>
2023-08-17 00:32:07 -0700

    length and first char

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,8 @@ if __name__ != "__main__":
 
 
 def multiple_returns(sentence):
-    if len(sentence) != 0:
-        return len(sentence), sentence[0]
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+        return (len(sentence), first_char)
