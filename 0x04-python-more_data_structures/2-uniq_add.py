799f15f2143dfd4f0831bb2a67d0bb20bf1c9ce0
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-18 23:54:33 +0100

    add unique ints

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def uniq_add(my_list=[]):
+    """Write a function that adds all unique integers in a list
+    (only once for each integer).
+
+        Args:
+        my_list: my list
+
+        Return: addition of unique ints
+    """
+    if len(my_list) == 0:
+        return []
+    my_list = list(set(my_list))
+    add = 0
+    for each_int in my_list:
+        add += each_int
+    return (add)
