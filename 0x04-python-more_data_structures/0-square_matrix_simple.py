606609465a88ab9dba4aa09081fda0a8f24b8bc0
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-18 22:35:33 +0100

    search and replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,7 +6,4 @@ if __name__ != "__main__":
 def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return None
-    result = []
-    for each_item in matrix:
-        result.append([each_int ** 2 for each_int in each_item])
-    return result
+    return [[each_int ** 2 for each_int in each_item] for each_item in matrix]
---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def search_replace(my_list, search, replace):
+    """function that replaces all occurrences of an element by another
+      in a new list.
+
+        Args:
+        my_list: the initial list
+        search: the element to replace in the list
+        replace: the new element
+
+        Return:
+        A new list
+    """
+    if len(my_list) == 0:
+        return None
+    if 2 not in my_list:
+        return my_list
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if new_list[i] == 2:
+            new_list[i] = 89
+    return new_list
