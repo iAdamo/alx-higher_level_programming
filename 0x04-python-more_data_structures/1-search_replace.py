752d177e76903b4383ece4d13a16e3d11fdc73bc
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-18 22:52:08 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -21,6 +21,6 @@ def search_replace(my_list, search, replace):
         return my_list
     new_list = my_list.copy()
     for i in range(len(my_list)):
-        if new_list[i] == 2:
-            new_list[i] = 89
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
