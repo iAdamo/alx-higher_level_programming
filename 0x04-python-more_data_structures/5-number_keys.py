7c4a66b3387c8f34b5c204f03e4a41ce886564ca
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-19 01:14:46 +0100

    key numbers in a dict

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def number_keys(a_dictionary):
+    """ function that returns the number of keys in a dictionary.
+
+        Args:
+        a_dictionary: a dictionary
+
+        Return:
+        the number of keys
+    """
+    return (len(a_dictionary.keys()))
