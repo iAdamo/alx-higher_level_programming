7b7a74693d2a1eabd32e531d545e0d5c0ed6a815
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-19 18:35:11 +0100

    Roman to Integer: fix cases test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """ function that converts a Roman numeral to an integer."""
-    if isinstance(roman_string, str):
-        num = []
-        result = int()
-        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                      'D': 500, 'M': 1000}
-        roman_list = list(roman_string.upper())
-        for figure in roman_list:
-            for key, value in roman_dict.items():
-                if figure == key:
-                    num.append(value)
-        for i in range(len(num)):
-            if i == 0:
-                result = num[i]
-            elif num[i] > num[i - 1]:
-                result = num[i] - result
-            else:
-                if i == (len(num) - 2) and (num[i + 1] > num[i]):
-                    result += (num[i + 1] - num[i])
-                    return (result)
-                result += num[i]
-        return (result)
+    if type(roman_string) is not str:
+        return (0)
+    num = []
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    roman_list = list(roman_string.upper())
+    for figure in roman_list:
+        for key, value in roman_dict.items():
+            if figure == key:
+                num.append(value)
+    for i in range(len(num)):
+        if i == 0:
+            result = num[i]
+        elif num[i] > num[i - 1]:
+            result = num[i] - result
+        else:
+            if i == (len(num) - 2) and (num[i + 1] > num[i]):
+                result += (num[i + 1] - num[i])
+                return (result)
+            result += num[i]
+    return (result)
