67346c473e4fbda0c846d9dce385f21946c03005
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-19 01:57:21 +0100

    print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+    exit
+
+
+def print_sorted_dictionary(a_dictionary):
+    if len(a_dictionary) == 0:
+        exit
+
+    for key, value in sorted(a_dictionary.items()):
+        print(f"{key}: {value}")
