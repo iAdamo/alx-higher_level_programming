75d608c624ff18cbd00f220d7de918497d38f717
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-19 18:24:14 +0100

    Roman to Integer: fix cases test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,22 @@
 def roman_to_int(roman_string):
     """ function that converts a Roman numeral to an integer."""
     if isinstance(roman_string, str):
-        result = 0
-        roman_figure = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                        'D': 500, 'M': 1000}
+        num = []
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                      'D': 500, 'M': 1000}
         roman_list = list(roman_string.upper())
-        for each_str in roman_list:
-            for key, value in roman_figure.items():
-                if each_str == key:
-                    result += value
-                    break
+        for figure in roman_list:
+            for key, value in roman_dict.items():
+                if figure == key:
+                    num.append(value)
+        for i in range(len(num)):
+            if i == 0:
+                result = num[i]
+            elif num[i] > num[i - 1]:
+                result = num[i] - result
+            else:
+                if i == (len(num) - 2) and (num[i + 1] > num[i]):
+                    result += (num[i + 1] - num[i])
+                    return (result)
+                result += num[i]
         return (result)
