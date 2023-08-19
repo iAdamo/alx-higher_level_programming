07ee2be6e0e449a897d1de7918a548c83c5ac418
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-08-19 20:08:17 +0100

    Squared by using map

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    """ Write a function that returns the weighted average of all
+        integers tuple
+
+        Args:
+        my_list: list of tuples
+
+        Return:
+        the weighted average of all integers tuple
+    """
     if len(my_list) == 0:
         return (0)
     score, weight = (0, 0)
