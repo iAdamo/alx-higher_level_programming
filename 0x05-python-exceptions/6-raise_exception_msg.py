cba9c80b13361436bf644d649fd5eb1ab0b53ff7
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-30 00:26:54 +0100

    Rais a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """raise_exception - function that raises a name exception with a message.
+
+    Args:
+        message: message string to be printed
+
+    Return:
+        Nothing"""
+    raise NameError(message)
