3216333306f49ad7521245b2f1619275081994a1
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-29 13:55:23 +0100

    Safe printing of an integers list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """safe_print_list - prints x elements of a list.
+
+    Args:
+        my_list: contain any type (integer, string, etc.)
+        x: epresents the number of elements to print
+
+    Return:
+        the real number of elements printed
+    """
     try:
         for i in range(x):
             print(my_list[i], end='')
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """safe_print_integer - prints an integer with "{:d}".format().
+
+    Args:
+        value: value can be any type
+
+    Return:
+        True if value has been correctly printed
+        (it means the value is an integer)
+        Otherwise, returns False
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError):
+        for each_item in value:
+            print("{:d}".format(each_item))
+        return (True)
+    except (ValueError):
+        return (False)
