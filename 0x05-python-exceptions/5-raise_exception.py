52faf6833a84989aa07d8d6385baadf9984e526e
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-30 00:06:20 +0100

    raise a typeerror exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception():
+    """raise_exception -  function that raises a type exception
+
+    Args:
+        void: Nothing
+
+    Return:
+        Nothing
+    """
+    raise TypeError
