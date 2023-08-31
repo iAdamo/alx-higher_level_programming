60b9aaae4fed26dfbd8db7b26414e132d483891c
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-31 17:20:01 +0100

    square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,4 +2,13 @@
 class Square:
     """Square - class Square that defines a square"""
     def __init__(self, size):
+        """__init__ - defines the attribute of an instance
+        
+        Args:
+            self: A reference to the object instance
+            size: size of the square
+
+        Return:
+            None
+        """
         self.__size = size
