991559e83b69bc248726eb8312d2a5da173e9c7e
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-08-31 10:46:27 +0100

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """Square - an empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+### **0x06. Python - Classes and Objects**
+**`Python`** **`OOP`**
