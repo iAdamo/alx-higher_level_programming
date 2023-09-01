99ffeca6c2add76d79a16bb23e1f9f1da92f5b08
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-01 20:06:02 +0100

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,7 +4,7 @@
 
 class Node:
     """class Node that defines a node of a singly linked list"""
-    def __init__(self, data, next_node=0):
+    def __init__(self, data, next_node=None):
         """initialization of instance attributes"""
         self.data = data
         self.next_node = next_node
