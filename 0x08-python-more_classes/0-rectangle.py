84eb051ec9961973842feff0f01f1625e4aa76df
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-05 08:53:28 +0100

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+0-rectangle.py module - contains Rectangle() class
+"""
+
+
+class Rectangle():
+    """Rectangle - mpty class Rectangle that defines a rectangle"""
+    pass
