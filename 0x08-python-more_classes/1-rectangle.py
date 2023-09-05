bd2e1e06241df0c09a4d46d699e8aba80ba763d5
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-05 09:18:01 +0100

    Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+0-rectangle.py module - contains Rectangle() class
+"""
+
+
+class Rectangle():
+    """Rectangle - mpty class Rectangle that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__heigth = height
+
+    @property
+    def width(self):
+        """getter peoperty width - returns private instance __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter property width - sets value to
+        private instance attribute __height"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter peoperty height - returns private instance __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter property width - sets value to
+        private instance attribute __height"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
