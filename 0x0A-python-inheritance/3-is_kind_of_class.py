448fea1d224d241c808a6466865e81125c6f7a2b
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-11 23:16:21 +0100

    is_kind_of_class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""2-is_same_class module: python inheritance
+"""3-is_kind_of_class module: python inheritance
 
 Test file for this module is located in ./tests/
 Run (python3 -m doctest -v ./tests/2-is_same_class.txt | tail -2) to test
@@ -7,9 +7,10 @@ Remove the pipeline to see full details
 """
 
 
-def is_same_class(obj, a_class):
-    """is_same_class - a function that returns True if the object is exactly
-    an instance of the specified class
+def is_kind_of_class(obj, a_class):
+    """returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class;
+    otherwise False
 
     Args:
         obj: unknown object
