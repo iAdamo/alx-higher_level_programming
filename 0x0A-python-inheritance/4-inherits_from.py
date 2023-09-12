cb3a349edbd21eac141729680f4c2b5cfa116a1c
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-12 01:45:12 +0100

    inherits from only sub class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""inherits_from module: python inheritance
+
+Test file for this module is located in ./tests/
+Run (python3 -m doctest -v ./tests/2-is_same_class.txt | tail -2) to test
+Remove the pipeline to see full details
+"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited (directly or indirectly) from,
+    the specified class; otherwise False
+
+    Args:
+        obj: unknown object
+        a_class: class object
+    """
+    return type(obj) is not a_class
