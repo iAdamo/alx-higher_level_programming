3dbcfa2f7622b165bf64040454b298596edd2fa4
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-12 03:39:10 +0100

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""The 5-base_geometry: python inheritance
+
+Test file for this module is located in ./tests/
+Run (python3 -m doctest -v ./tests/7-base_geometry.txt | tail -2) to test
+Remove the pipeline to see full details
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate the value input"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/jk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git add .
-git add ..
-
-ech0 -n read $1
-
-git commit -m "{message}"
-
-git push
