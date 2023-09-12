0e97bd334784b4a2eeb84adf452bb3d87dee72af
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-12 02:19:47 +0100

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The 5-base_geometry: python inheritance
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
