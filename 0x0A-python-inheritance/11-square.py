48ae20c3fa7423e438cab6fa46355f5bdf95141e
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-12 06:39:03 -0700

    Square#2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -18,4 +18,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """returns an informal represantation of an instance"""
-        return f"[Rectangle] {self.__size}/{self.__size}"
+        return f"[Square] {self.__size}/{self.__size}"
