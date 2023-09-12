94cee728eb6b754f652cb9363c23a19881eae335
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-12 05:07:05 -0700

    fixing issubclass error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
         """validate the value input"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""The 5-base_geometry: python inheritance
+"""The 8-rectangle: python inheritance
 
 Test file for this module is located in ./tests/
 Run (python3 -m doctest -v ./tests/7-base_geometry.txt | tail -2) to test
 Remove the pipeline to see full details
 """
-
-
-class BaseGeometry():
-    """class BaseGeometry"""
-    def area(self):
-        """raise an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validate the value input"""
-        if not isinstance(value, int) or type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """child class Rectangle to parent class BaseGeometry"""
     def __init__(self, width, height):
         """initialize an object instance"""
-        self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-        super().integer_validator("height", height)
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
+        self.__width = width
