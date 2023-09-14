4085a2abdd07e0b1b286460581900322cacf112e
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-14 15:38:22 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This is the `9-student` module
+It supplies `Student` class
+Methods:
+     `to_json`
+     `reload_from_json`
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize instances to class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        new_dict = {}
+        my_dict = vars(self)
+        if not isinstance(attrs, list):
+            return vars(self)
+        for each_attr in attrs:
+            if isinstance(each_attr, str):
+                for key, value in my_dict.items():
+                    if key is each_attr:
+                        new_dict[key] = value
+            else:
+                return vars(self)
+        return new_dict
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
