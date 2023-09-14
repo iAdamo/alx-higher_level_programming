279877548ab3daaaac57fdbd32685dac68a801e4
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-09-14 06:34:57 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """
-7-add_item module
-save_to_json_file function; 5-save_to_json_file
-load_from_json_file function; 6-load_from_json_file
-argv function; sys module
+script that adds all arguments to a Python list, and then save them to a file
+
+Usage:
+    add_item arg1 arg2 ...
+
+If the file doesn’t exist, it should be created
+load_from_json_file: loads the file for deserializing
+command line args are made into list
+load_from_json_file: list are saved back into the file by serializing them
 """
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 lists = []
-if len(argv) == 1:
+if len(argv) > 1:
+    lists = load_from_json_file('add_item.json')
+
+    for i in range(1, len(argv)):
+        lists.append(argv[i])
+
     save_to_json_file(lists, 'add_item.json')
-    exit(1)
-lists = load_from_json_file('add_item.json')
-for i in range(1, len(argv)):
-    lists.append(argv[i])
-save_to_json_file(lists, 'add_item.json')
+else:
+    save_to_json_file(lists, 'add_item.json')
