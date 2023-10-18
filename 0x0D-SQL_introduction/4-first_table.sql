2c30692ac1c726752dbcc13a3335c097ac561825
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-10-18 19:38:56 +0100

    first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- script that creates a table called first_table in the current data
+-- base in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
+DESCRIBE first_table;
