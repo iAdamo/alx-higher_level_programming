df18fe9172f1cdf805aa1d6b82d8cf61c3009173
Adam_Sanusi_Babatunde <iadamo.inc@gmail.com>
2023-10-19 22:15:49 +0100

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
