37ac40d46c9bc53da735d5f615f32ff50fc151eb
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-14 21:23:21 +0100

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * script that prints two arguments passed to it, in the following format: “ is ”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ */
+console.log(`${process.argv[2]}` + ' is ' + `${process.argv[3]}`);
