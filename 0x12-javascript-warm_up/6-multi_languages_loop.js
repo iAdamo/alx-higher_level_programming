a4c57af01a8baf21386ae1c78992e405f192bba0
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-15 03:44:51 +0100

    updated: for...of loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,6 @@
 /*
  * script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
  */
-const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myArr.length; i++) {
-  console.log(myArr[i]);
+for (const str of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(str);
 }
