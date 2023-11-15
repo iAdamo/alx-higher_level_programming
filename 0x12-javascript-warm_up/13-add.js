e032a654cd4dd78247de0fac961984c176d3d436
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-15 10:03:52 +0100

    exporting a named function (add)

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * function that returns the addition of 2 integers.
+ *
+ * Run `./mainttest/13-main.js` to test
+ */
+exports.add = function add (num1, num2) {
+  return (num1 + num2);
+};
---
+++ 0x12-javascript-warm_up/mainttest/13-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/**
+ * make sure to modify the pathname if pathname changes
+ */
+const add = require('../13-add').add;
+console.log(add(3, 5));
