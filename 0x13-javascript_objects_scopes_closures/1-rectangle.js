fe86077951615de4beae283d7b645e0cc6fc580c
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-16 03:23:21 +0100

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * A class `Rectangle` that defines a rectangle
+ *
+ * Run `./maintest/1-main.js` to test
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
