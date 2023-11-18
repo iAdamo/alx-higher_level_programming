818f9ab360a0193e5829970a4a3726c60843ca64
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-18 20:06:23 +0100

    Get all states | port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -21,10 +21,10 @@ from sys import argv
 # Connects to a MySQL server running on localhost at port 3306.
 db = MySQLdb.connect(
     host='localhost',
-    port=3306,
     user=argv[1],
     passwd=argv[2],
-    db=argv[3]
+    db=argv[3],
+    port=3306
 )
 
 # Creates a cursor handle
