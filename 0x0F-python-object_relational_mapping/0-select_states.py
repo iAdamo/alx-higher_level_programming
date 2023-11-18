e8485b00370768b174e6f41c032ebcb9871bd4e0
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-18 22:18:31 +0100

    Get all states | adjusted docstrings

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa
-    The script takes 3 arguments: mysql username, mysql password and database
-    name (no argument validation needed)
-    Use the module MySQLdb (import MySQLdb)
-    Connect to a MySQL server running on localhost at port 3306
-    Results must be sorted in ascending order by states.id
-    Results must be displayed as they are in the example below
-    The code should not be executed when imported
 Use `source maintest/0-select_states.sql` to load into the MySQL Server
-Execute by ./0-select_states.py root root hbtn_0e_0_usa
+Execute by ./0-select_states.py username password database_name
 """
+# make sure the code is not executed when imported
 if __name__ != "__main__":
     exit()
 
+# Import necessary modules
 import MySQLdb
 from sys import argv
 
+# Connects to a MySQL server running on localhost at port 3306.
 db = MySQLdb.connect(
     host='localhost',
     user=argv[1],
@@ -24,14 +20,19 @@ db = MySQLdb.connect(
     port=3306
 )
 
+# Creates a cursor handle
 ptr = db.cursor()
 
+# Executes the SQL query
 ptr.execute('SELECT * FROM states ORDER BY id ASC')
 
-table = ptr.fetchall()
+# Fetches all rows from the result of the query
+rows = ptr.fetchall()
 
-for row in table:
+# Prints the rows
+for row in rows:
     print(row)
 
+# Clean up
 ptr.close()
 db.close()
