1580bab4f1105c4fd20d6187f7a659ea0912f2dd
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 02:23:00 +0100

    Get all states | fixing checker 4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+Use (source maintest/0-select_states.sql) to load into the MySQL Server
+Execute by (./0-select_states.py username password database_name)
 """
-script that lists all states from the database hbtn_0e_0_usa
-Use `source maintest/0-select_states.sql` to load into the MySQL Server
-Execute by ./0-select_states.py username password database_name
-"""
-
-
+# make sure the code is not executed when imported
 if __name__ != "__main__":
     exit()
 
+# Import necessary modules
 import MySQLdb
 from sys import argv
 
+# Connects to a MySQL server running on localhost at port 3306.
 db = MySQLdb.connect(
     host='localhost',
     user=argv[1],
@@ -19,10 +19,20 @@ db = MySQLdb.connect(
     db=argv[3],
     port=3306
 )
+
+# Creates a cursor handle
 ptr = db.cursor()
+
+# Executes the SQL query
 ptr.execute('SELECT * FROM states ORDER BY id ASC')
+
+# Fetches all rows from the result of the query
 rows = ptr.fetchall()
+
+# Prints the rows
 for row in rows:
     print(row)
+
+# Clean up
 ptr.close()
 db.close()
