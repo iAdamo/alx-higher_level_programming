d21b9a2f471366c0f15e550110c87b0adb287fd4
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 11:49:04 +0100

    Cities by states | Using INNER JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
         db=argv[3]
     )
     ptr = db.cursor()
-    ptr.execute("SELECT * FROM cities "
+    ptr.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "INNER JOIN states ON cities.state_id = states.id "
                 "ORDER BY cities.id ASC")
     rows = ptr.fetchall()
     for row in rows:
