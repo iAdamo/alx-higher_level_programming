d3c75aa2e547bc0090d605358b60c3b68199985d
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-18 23:09:36 -0800

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+Use (source maintest/0-select_states.sql) to load into the MySQL Server
+Execute by (./1-select_states.py username password database_name)
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    ptr = db.cursor()
+    ptr.execute('SELECT * FROM states '
+                'WHERE name LIKE "n%" '
+                'ORDER BY id ASC')
+    rows = ptr.fetchall()
+    for row in rows:
+        print(row)
+    ptr.close()
+    db.close()
