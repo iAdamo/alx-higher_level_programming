1a64f67d0336737b1e21daa9767f155a89c02a02
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 11:28:50 +0100

    Cities by states | cities_by_state.sql

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that lists all cities from the database hbtn_0e_4_usa
+Use (source maintest/0-select_cities.sql) to load into the MySQL Server
+Execute by (./4-cities_by_state.py username password database_name)
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    ptr = db.cursor()
+    ptr.execute("SELECT * FROM cities "
+                "ORDER BY cities.id ASC")
+    rows = ptr.fetchall()
+    for row in rows:
+        print(row)
+    ptr.close()
+    db.close()
