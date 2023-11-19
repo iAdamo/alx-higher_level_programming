c36618a669acba8ae5752beb2d843327d6532c9a
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 13:11:54 +0100

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,7 +2,7 @@
 """ script that takes in the name of a state as an argument and lists all
 cities of that state, using the database hbtn_0e_4_usa
 Use (source maintest/0-select_cities.sql) to load into the MySQL Server
-Execute by (./5-filter_cities.py username password database_name)
+Execute by (./5-filter_cities.py username password database_name statename)
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -16,12 +16,12 @@ if __name__ == "__main__":
         db=argv[3]
     )
     ptr = db.cursor()
-    ptr.execute("SELECT cities.name FROM cities "
+    ptr.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "INNER JOIN states ON cities.state_id = states.id "
                 "WHERE states.name LIKE BINARY %s "
                 "ORDER BY cities.id ASC", (argv[4],))
     rows = ptr.fetchall()
     for row in rows:
-        print(row[0], end=", " if row != rows[-1] else "\n")
+        print(row[1], end=", " if row != rows[-1] else "\n")
     ptr.close()
     db.close()
