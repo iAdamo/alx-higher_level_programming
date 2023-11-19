5e54f367249f0009a0cd869f6970741159fcbbd1
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 00:19:18 -0800

    task 2 comma error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
+        db=argv[3]
     )
     ptr = db.cursor()
     ptr.execute('SELECT * FROM states '
