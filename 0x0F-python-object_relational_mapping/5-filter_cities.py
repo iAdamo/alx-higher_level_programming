600b8734e006bac656a370364c359b54fca6d86f
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-19 13:29:43 +0100

    comment

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python3
-""" script that takes in the name of a state as an argument and lists all
-cities of that state, using the database hbtn_0e_4_usa
-Use (source maintest/0-select_cities.sql) to load into the MySQL Server
-Execute by (./5-filter_cities.py username password database_name statename)
 """
+This script takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa.
+It is executed by running: ./5-filter_cities.py username password database_name
+statename
+"""
+
+# This condition ensures the code only runs when the script is run directly,
+# not when imported
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
+    # Establish a connection to the MySQL database
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=argv[1],  # username from command line argument
+        passwd=argv[2],  # password from command line argument
+        db=argv[3]  # database name from command line argument
     )
+
+    # Create a cursor object to interact with the database
     ptr = db.cursor()
+
+    # Execute a SQL query to fetch cities from a specific state
     ptr.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "INNER JOIN states ON cities.state_id = states.id "
                 "WHERE states.name LIKE BINARY %s "
-                "ORDER BY cities.id ASC", (argv[4],))
+                "ORDER BY cities.id ASC", (argv[4],))  # state name from CL arg
+
+    # Fetch all rows returned by the query
     rows = ptr.fetchall()
+
+    # Loop through each row
     for row in rows:
+        # Print the city name (second element in the row)
+        # If it's the last row, end with a newline, otherwise end with a comma
+        # and space
         print(row[1], end=", " if row != rows[-1] else "\n")
+
+    # Close the cursor and the database connection
     ptr.close()
     db.close()
