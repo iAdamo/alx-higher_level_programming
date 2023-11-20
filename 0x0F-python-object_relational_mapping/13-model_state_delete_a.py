6d95b5465dda3185464a1d2535ec2d203850669a
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-20 13:48:26 +0100

    Delete states

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This script changes the name of a State object from the database hbtn_0e_6_usa
+It takes 3 arguments: mysql username, mysql password, and database name.
+"""
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 4:
+        print('Usage: argv[0] <username> <password> <database>',
+              file=sys.stderr)
+        exit()
+
+    # Create a SQLAlchemy engine that provides a source of database
+    # connectivity. This engine connects to the MySQL server using the
+    # provided username, password, and database name.
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1],  # username
+            sys.argv[2],  # password
+            sys.argv[3]   # database name
+        ),
+        pool_pre_ping=True
+    )
+
+    # Create all tables in the engine. This is akin to the "CREATE TABLE"
+    # statement in raw SQL. It uses the metadata from the Base class to
+    # determine the structure of the tables.
+    Base.metadata.create_all(engine)
+
+    # Create a new session for database interaction
+    session = Session(engine)
+
+    state_to_delete = session.query(State).filter(State.name.contains('a'))
+
+    state_to_delete.delete()
+
+    session.commit()
+
+    session.close()
