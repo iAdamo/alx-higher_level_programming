2ae9f6d6e061b349942e6e5310f2d285ebcf5fc7
Adam Sanusi Babatunde <tundey520@gmail.com>
2023-12-03 22:25:54 -0800

    Find a peak using binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,10 +12,15 @@ def find_peak(list_of_integers):
     Return:
         (Int)The peak
     """
-    if len(list_of_integers) == 0:
+    if not list_of_integers:
         return None
-    peak = list_of_integers[0]
-    for num in list_of_integers:
-        if num > peak:
-            peak = num
-    return peak
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return list_of_integers[left]
