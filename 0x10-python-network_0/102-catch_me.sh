ae0610d71225a1eb9c8aad0c77e92a0a05be8d49
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-06 19:19:47 +0900

    catch me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
