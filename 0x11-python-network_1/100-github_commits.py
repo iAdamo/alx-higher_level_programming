5aa6592a60e243eba6e01d947223cb654e71f5d9
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-07 22:54:02 -0800

    listing recent commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
         sys.argv[2], sys.argv[1])
 
     commits = requests.get(url).json()
-    for i in range(10):
-        print(f"{commits[i].get('sha')}: "
-              f"{commits[i].get('commit').get('author').get('name')}")
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
