b508631bac1361db0400a0d05b04a66831321487
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-12-07 20:58:13 -0800

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) == 2 else ""}
+
+    try:
+        response = requests.post(url, data=data).json()
+        if response:
+            print(f"[{response.get('id')}] {response.get('name')}")
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodError:
+        print("Not a valid JSON")
