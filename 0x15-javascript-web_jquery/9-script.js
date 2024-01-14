bd81c9316b8d4a3ad9b23baa2320e037e76ac3fc
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-01-14 18:23:35 +0100

    Fetch Hello!

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+/**
+ * Script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.
+ * The translation of “hello” must be displayed in the HTML tag DIV#hello
+ * You can’t use document.querySelector to select the HTML tag
+ * You must use the JQuery API
+ * Your script must work when it is imported from the <head> tag
+ */
+
+$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', (data) => {
+  $('DIV#hello').text(data.hello);
+});
